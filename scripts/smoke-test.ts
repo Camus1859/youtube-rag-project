5b3da694427944ec0ba2258f3f0ef2879493da89
Anderson Laventure <Laventurepsu1@gmail.com>
2026-05-29 20:17:55 -0400

    feat: enhance smoke test with Pinecone integration and environment variable checks

--- scripts/smoke-test.ts
+++ scripts/smoke-test.ts
@@ -1,5 +1,18 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+import "dotenv/config"
+
 const PRODUCTION_URL = process.env.PRODUCTION_URL
+const TEST_CHANNEL = 'https://www.youtube.com/@SportsRadio94WIP'
+const TEST_NAMESPACE = 'SportsRadio94WIP'
 
 if(!PRODUCTION_URL){
     throw new Error(" URL is missing")
 }
+
+if (!process.env.PINECONE_API_KEY) throw new Error('PINECONE_API_KEY is not defined')
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY })
+
+const pcIndex = pc.index('standard-dense-js')
+await pcIndex.namespace(TEST_NAMESPACE).deleteAll()
+
+
